4981ea0351367919d60aa5acfb69035491c2fbf7
jsonlog <jsonlogit@gmail.com>
2019-04-25 17:20:54 +0800

    springInitializr

---
+++ src/main/java/json/log/myspringblog/MyspringblogApplication.java
@@ -0,0 +1,13 @@
+package json.log.myspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyspringblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/json/log/myspringblog/MyspringblogApplicationTests.java
@@ -0,0 +1,16 @@
+package json.log.myspringblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyspringblogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
