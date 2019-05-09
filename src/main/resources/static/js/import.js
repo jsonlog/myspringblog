4232b89cc7b81f3d6743109790ff2773421d0d3a
jsonlog <jsonlogit@gmail.com>
2019-05-09 18:52:17 +0800

    js

--- src/main/resources/static/js/import.js
+++ src/main/resources/static/js/import.js
@@ -1,4 +1,4 @@
-document.write('<title>首页_老G博客网站</title>');
+document.write('<title>老G博客网站</title>');
 document.write('<meta name="keywords" content="个人博客,老G个人博客,个人博客模板,老G" />');
 document.write('<meta name="description" content="老G个人博客，是一个支持springboot个人原创网站。" />');
 document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
@@ -9,3 +9,8 @@ document.write('<script src="./../js/jquery.min.js" ></script>');
 document.write('<script src="./../js/hc-sticky.js" ></script>');
 document.write('<script src="./../js/comm.js" ></script>');
 // document.write('<script src="./../js/modernizr.js" ></script>');
+// $(document).ready()(function ltrb(){
+// });
+// $("#header").load("./../header.html");
+// $("#aside").load("./../aside.html");
+// $("#footer").load("./../footer.html");
