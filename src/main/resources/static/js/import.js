cf2773c41b671c06f8b96208f0b745a59eca7587
jsonlog <jsonlogit@gmail.com>
2019-05-09 18:17:13 +0800

    import

---
+++ src/main/resources/static/js/import.js
@@ -0,0 +1,11 @@
+document.write('<title>首页_老G博客网站</title>');
+document.write('<meta name="keywords" content="个人博客,老G个人博客,个人博客模板,老G" />');
+document.write('<meta name="description" content="老G个人博客，是一个支持springboot个人原创网站。" />');
+document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
+document.write('<link href="./../css/base.css" rel="stylesheet">');
+document.write('<link href="./../css/index.css" rel="stylesheet">');
+document.write('<link href="./../css/m.css" rel="stylesheet">');
+document.write('<script src="./../js/jquery.min.js" ></script>');
+document.write('<script src="./../js/hc-sticky.js" ></script>');
+document.write('<script src="./../js/comm.js" ></script>');
+// document.write('<script src="./../js/modernizr.js" ></script>');
